6965ad4b7312f71639ba91607e1556fc7718f0c2
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-05 15:23:25 +0500

    task5 added

--- NodeJs-4/hw4.js
+++ NodeJs-4/hw4.js
@@ -65,30 +65,46 @@ const textArea = document.querySelector(".textarea");
 // btnAdd.addEventListener("click", () => sendValue());
 //--------------------------------------------------------------------------------
 
-const sendValue = () => {
-  let T = 0.5;
-  let g = 9.8;
-  // let N = 3;
-  textArea.innerHTML = `
- <table width="50%" border="1">
-  <thead>
-    <tr>
-      <th>Vaqt</th>
-      <th>Tezlik</th>
-    </tr>
-  </thead>
-  <tbody class="tbodyT2"></tbody>
-</table>`;
-  const tbodyT2 = document.querySelector(".tbodyT2");
-  let val = in1.value;
-  for (let i = 0; i <= val; i += T) {
-    tbodyT2.innerHTML += `<tr>
-      <td>${i}</td>
-      <td>${(v = i * g).toFixed(2)}</td>
-      </tr>`;
-  }
+// const sendValue = () => {
+//   let T = 0.5;
+//   let g = 9.8;
+//   // let N = 3;
+//   textArea.innerHTML = `
+//  <table width="50%" border="1">
+//   <thead>
+//     <tr>
+//       <th>Vaqt</th>
+//       <th>Tezlik</th>
+//     </tr>
+//   </thead>
+//   <tbody class="tbodyT2"></tbody>
+// </table>`;
+//   const tbodyT2 = document.querySelector(".tbodyT2");
+//   let val = in1.value;
+//   for (let i = 0; i <= val; i += T) {
+//     tbodyT2.innerHTML += `<tr>
+//       <td>${i}</td>
+//       <td>${(v = i * g).toFixed(2)}</td>
+//       </tr>`;
+//   }
 
-  in1.value = "";
-};
+//   in1.value = "";
+// };
 
-btnAdd.addEventListener("click", () => sendValue());
+// btnAdd.addEventListener("click", () => sendValue());
+
+//-----------------------------------------------------------------------------------
+//№5
+// const sendValue = () => {
+//   let N = in1.value;
+//   let sum = 0;
+//   for (let i = 0; i < N; i++) {
+//     textArea.innerHTML += `<p>${(sum += i)}</p>`;
+//   }
+
+//   in1.value = "";
+// };
+
+// btnAdd.addEventListener("click", () => sendValue());
+
+//------------------------------------------------------------------------------------
