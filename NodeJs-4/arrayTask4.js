bf5ed1703f833c2b0e620b8a07620cdbb6c87f38
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-05 16:31:00 +0500

    start array tasks

---
+++ NodeJs-4/arrayTask4.js
@@ -0,0 +1,12 @@
+const in1 = document.getElementById("in1");
+const btnAdd = document.getElementById("btnAdd");
+const textArea = document.querySelector(".textarea");
+
+//#1
+
+// let arr = [];
+
+// for (let i = 0; i < 10; i++) {
+//   arr.push(Math.ceil(Math.random() * 100));
+// }
+// console.log(arr);
