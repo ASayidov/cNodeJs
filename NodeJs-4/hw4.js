dbbaddd1f51e5aa6302b38fc91383cd2a6bc7c35
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-05 14:34:31 +0500

    added task2

--- NodeJs-4/hw4.js
+++ NodeJs-4/hw4.js
@@ -2,10 +2,49 @@ const in1 = document.getElementById("in1");
 const btnAdd = document.getElementById("btnAdd");
 const textArea = document.querySelector(".textarea");
 
+// const sendValue = (count = 10) => {
+//   for (let i = 0; i < count; i++) {
+//     textArea.innerHTML += `<p>${in1.value}</p>`;
+//   }
+//   in1.value = "";
+// };
+
+// btnAdd.addEventListener("click", () => sendValue());
+//-----------------------------------------------------------------------
+
+const tbodyT2 = document.querySelector(".tbodyT2");
+
 const sendValue = () => {
-  let ismCount = 10;
-  for (let i = 0; i < ismCount; i++) {
-    textArea.innerHTML += `<p>${in1.value}</p>`;
+  let val = in1.value;
+  for (let i = 1; i <= val; i++) {
+    tbodyT2.innerHTML += `<tr>
+      <td>${i}</td>
+      <td>${Math.pow(i, 2)}</td>
+      </tr>`;
   }
+
   in1.value = "";
 };
+
+btnAdd.addEventListener("click", () => sendValue());
+
+//------------------------------------------------------------------------------
+
+// 2.Jadvalda berilgan N sonigacha bo'lgan sonlarni kvadratini chiqaring
+// var N = 8
+// -----------------------------------
+// Son                   Kvadrat
+// -----------------------------------
+// 1                            1
+// 2                            4
+// 3                            9
+// 4                           16
+// 5                           25
+// 6                           36
+// 7                           49
+// 8                           64
+
+// let N = 8;
+// for (let i = 1; i <= N; i++) {
+//   console.log(i, Math.pow(i, 2));
+// }
