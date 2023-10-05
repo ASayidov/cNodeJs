d7f2fa63ca9f98fe0fd2a0f4bf34f83cd9f418a9
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-05 11:30:31 +0500

    update css

--- NodeJs-4/hw4.js
+++ NodeJs-4/hw4.js
@@ -1,8 +1,9 @@
-const in1 = document.getElementById("in1")
-const btnAdd = document.getElementById("btnAdd")
-const textP = document.getElementById("textP")
+const in1 = document.getElementById("in1");
+const btnAdd = document.getElementById("btnAdd");
+const textP = document.getElementById("textP");
 
-const sendValue = ()=> {
-  textP.textContent=in1.value
-  in1.value=""
-}
+const sendValue = () => {
+  textP.textContent = in1.value;
+  in1.value = "";
+};
+
