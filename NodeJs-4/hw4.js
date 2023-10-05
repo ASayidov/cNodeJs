9654c5508029a1a733452f71d4ef72a96bda3e11
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-05 15:02:06 +0500

    task2 finished

--- NodeJs-4/hw4.js
+++ NodeJs-4/hw4.js
@@ -12,21 +12,30 @@ const textArea = document.querySelector(".textarea");
 // btnAdd.addEventListener("click", () => sendValue());
 //-----------------------------------------------------------------------
 
-const tbodyT2 = document.querySelector(".tbodyT2");
+// const sendValue = () => {
+//   textArea.innerHTML = `
+//  <table width="50%" border="1">
+//   <thead>
+//     <tr>
+//       <th>Son</th>
+//       <th>Kvadrat</th>
+//     </tr>
+//   </thead>
+//   <tbody class="tbodyT2"></tbody>
+// </table>`;
+//   const tbodyT2 = document.querySelector(".tbodyT2");
+//   let val = in1.value;
+//   for (let i = 1; i <= val; i++) {
+//     tbodyT2.innerHTML += `<tr>
+//       <td>${i}</td>
+//       <td>${Math.pow(i, 2)}</td>
+//       </tr>`;
+//   }
 
-const sendValue = () => {
-  let val = in1.value;
-  for (let i = 1; i <= val; i++) {
-    tbodyT2.innerHTML += `<tr>
-      <td>${i}</td>
-      <td>${Math.pow(i, 2)}</td>
-      </tr>`;
-  }
+//   in1.value = "";
+// };
 
-  in1.value = "";
-};
-
-btnAdd.addEventListener("click", () => sendValue());
+// btnAdd.addEventListener("click", () => sendValue());
 
 //------------------------------------------------------------------------------
 
