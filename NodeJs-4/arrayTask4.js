5e7e8d307d156c2cdd269095c18023dab84bc3c1
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-07 08:46:19 +0500

    upd tasks

--- NodeJs-4/arrayTask4.js
+++ NodeJs-4/arrayTask4.js
@@ -1,12 +1,36 @@
-const in1 = document.getElementById("in1");
-const btnAdd = document.getElementById("btnAdd");
-const textArea = document.querySelector(".textarea");
-
 //#1
 
-// let arr = [];
-
-// for (let i = 0; i < 10; i++) {
-//   arr.push(Math.ceil(Math.random() * 100));
+// function sendVal(n) {
+//   let arr = [];
+//   for (let i = 0; i < n; i++) {
+//     arr.push(Math.ceil(Math.random() * 100));
+//   }
+//   textArea.innerHTML = `<p>[${arr}]</p>`;
 // }
-// console.log(arr);
+
+// btnAdd.addEventListener("click", () => sendVal(in1.value));
+
+//#2----------------------------------------------------------------------------------
+
+// function sendVal(n) {
+//   let arr = [];
+
+//   for (let i = 0; i < n; i++) {
+//     arr.push("#");
+//   }
+//   textArea.innerHTML += `<p>${arr}</p>`;
+// }
+
+// btnAdd.addEventListener("click", () => sendVal(in1.value));
+
+//#3-------------------------------------------------------------------------------
+function sendVal(n) {
+  let arr = [];
+
+  for (let i = 0; i < n; i++) {
+    arr.push("#");
+  }
+  textArea.innerHTML += `<p>${arr}</p>`;
+}
+
+btnAdd.addEventListener("click", () => sendVal(in1.value));
