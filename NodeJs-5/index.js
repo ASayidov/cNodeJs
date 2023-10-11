44c7312f3dcef7b82249393edc92364c76f71ac8
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-11 07:27:37 +0500

    added render and get

--- NodeJs-5/index.js
+++ NodeJs-5/index.js
@@ -3,16 +3,40 @@
 // - texnika mahsulotlarni qo'shish va o'qish.
 // - maydonlar: mahsulot nomi, turi, narhi, skidka, umumiy narhi
 
+const tbody = document.querySelector("tbody");
+
 const inputs = document.querySelectorAll("#form [name]");
 const url = "http://localhost:3000/technicals";
+let techs = [];
+
+const render = () => {
+  tbody.innerHTML;
+  techs.forEach((item) => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${item.id}</td>
+        <td>${item.fname}</td>
+        <td>${item.year}</td>
+        <td>${item.techType}</td>
+      </tr>
+    `;
+  });
+};
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((res) => {
+    techs = [...res];
+    render();
+  });
 
 const addTech = () => {
   const tech = {};
 
-  console.log(tech);
   inputs.forEach((e) => {
     tech[e.getAttribute("name")] = e.value;
   });
+  console.log(JSON.stringify(tech));
   fetch(url, {
     method: "POST",
     headers: {
@@ -21,5 +45,7 @@ const addTech = () => {
     body: JSON.stringify(tech),
   })
     .then((res) => res.json())
-    .then((newTech) => console.log(newTech));
+    .then((newTech) => {
+      console.log(newTech);
+    });
 };
