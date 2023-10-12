67ce45d01f13acc9cdcc027994b6d0d8c0851f2d
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-12 16:23:15 +0500

    start todolist

---
+++ NodeJs-6/todo.js
@@ -0,0 +1,59 @@
+const input = document.getElementById("input-create");
+const form = document.getElementById("form-create");
+const url = "http://localhost:3000/todos";
+
+const btn = document.getElementsByTagName("button");
+const tbody = document.querySelector("tbody");
+
+let todos = [];
+
+const render = () => {
+  tbody.innerHTML = "";
+  todos.forEach((item, i) => {
+    tbody.innerHTML += `
+    <tr>
+    <td>${i + 1}</td>
+    <td>${item.input}</td>
+    <td><button onclick="editTodo(${item.id})">edit</button></td>
+    <td><button onclick="delTodo(${item.id})">delete</button></td>
+     </tr>`;
+  });
+};
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((res) => {
+    todos = [...res, todos];
+    console.log(res);
+    render();
+  });
+
+function editTodo(id) {
+  console.log(id);
+}
+
+function delTodo(id) {
+  fetch(`${url}/${id}`, {
+    method: "delete",
+  }).then(() => {
+    todos = todos.filter((e) => e.id !== id);
+  });
+}
+
+const addTodos = () => {
+  let todo = {};
+  todo[input.getAttribute("name")] = input.value;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  })
+    .then((res) => res.json())
+    .then((newTodos) => {
+      todo = [newTodos, ...todo];
+    });
+  render();
+};
