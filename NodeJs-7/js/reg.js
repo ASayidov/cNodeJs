b1cce921dd5e1d028dd167f24534e8e89f83fa2a
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-15 15:35:49 +0500

    less 7 added reg and login pages

--- NodeJs-7/js/reg.js
+++ NodeJs-7/js/reg.js
@@ -1,6 +1,31 @@
-const register = (e) => {
+const auth = (e, type = null) => {
   e.preventDefault();
+  let url = "https://hospital.bookblogs.uz";
   let form = e.target;
   let data = new FormData(form);
-  data.forEach((value, name) => console.log(value, name));
+  let user = {};
+  data.forEach((value, name) => (user[name] = value));
+  console.log(user);
+
+  if (type == "login") {
+    axios
+      .post(`${url}/auth/login`, user)
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("hospital-token", res.data.token);
+        localStorage.setItem("hospital-user", JSON.stringify(res.data.user));
+        alert("Xush kelibsiz");
+        window.location.href = "index.html";
+      })
+      .catch((e) => alert(e.response.data));
+  } else {
+    axios.post(`${url}/auth/reg`, user).then((res) => {
+      console.log(res.data);
+      if (res.data == "success") {
+        window.location.href = "login.html";
+      } else if (res.data == "exist") {
+        alert("Bunday loginli foydalanuvchi mavjud");
+      }
+    });
+  }
 };
