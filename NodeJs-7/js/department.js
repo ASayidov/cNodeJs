151763732a29d587e531c23732889b6079d94c88
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-16 21:23:21 +0500

    added post and get new depart

--- NodeJs-7/js/department.js
+++ NodeJs-7/js/department.js
@@ -1,8 +1,25 @@
 const tbody = document.querySelector("tbody");
+let departments = [];
 const render = () => {
   tbody.innerHTML = "";
-  
+  departments.forEach((el, i) => {
+    tbody.innerHTML += `
+        <tr>
+            <th scope="row">${i + 1}</th>
+            <td>${el.title}</td>
+            <td>${el.createdTime}</td>
+            <td>${el.status}</td>
+            <td>${el.status}</td>
+            <td>${el.status}</td>
+          </tr>
+    `;
+  });
 };
+
+axios.get(`${url}/department`, header).then((res) => {
+  departments = [...res.data];
+  render();
+});
 
 const addDepart = (e) => {
   e.preventDefault();
@@ -12,4 +29,9 @@ const addDepart = (e) => {
   let department = {};
   data.forEach((value, name) => (department[name] = value));
   console.log(department);
+
+  axios.post(`${url}/department`, department, header).then((res) => {
+    departments = [res.data, ...departments];
+    form.reset();
+  });
 };
