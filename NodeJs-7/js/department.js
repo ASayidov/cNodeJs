8ad3d438365abfa93f41df37fac4cfb70b550da5
Abdullajon Sayidov <abdullajonsayidov@gmail.com>
2023-10-17 07:30:39 +0500

    added delete method and showed alert messages

--- NodeJs-7/js/department.js
+++ NodeJs-7/js/department.js
@@ -1,5 +1,7 @@
 const tbody = document.querySelector("tbody");
+const info = document.getElementById("info") || null;
 let departments = [];
+let department = {};
 const render = () => {
   tbody.innerHTML = "";
   departments.forEach((el, i) => {
@@ -9,12 +11,12 @@ const render = () => {
             <td>${el.title}</td>
             <td>${el.createdTime}</td>
             <td>${el.status}</td>
-            <td><button class="btn btn-info" onclick="editDep(${
-              el.id
-            })"><i class="bi bi-pencil"></i></button></td>
-              <td><button  class="btn btn-danger" onclick="delDep(${
-                el.id
-              })"><i class="bi bi-trash"></i></button></td>
+            <td><button class="btn btn-info" onclick="editDep('${
+              el._id
+            }')"><i class="bi bi-pencil"></i></button></td>
+              <td><button  class="btn btn-danger" onclick="delDep('${
+                el._id
+              }')"><i class="bi bi-trash"></i></button></td>
           </tr>
     `;
   });
@@ -25,16 +27,36 @@ axios.get(`${url}/department`, header).then((res) => {
   render();
 });
 
+const delDep = (id) => {
+  if (confirm("Qaroringiz qat'iymi?")) {
+    axios.delete(`${url}/department/${id}`, header).then((res) => {
+      console.log(res.data);
+      departments = departments.filter((dep) => dep._id !== id);
+      render();
+      info.innerHTML = `
+      <div class="alert alert-warning" role="alert">Bo'lim o'chirildi</div>`;
+      setTimeout(() => {
+        info.innerHTML = "";
+      }, 2000);
+    });
+  }
+};
+
 const addDepart = (e) => {
   e.preventDefault();
 
   let form = e.target;
   let data = new FormData(form);
-  let department = {};
+  //let department = {};
   data.forEach((value, name) => (department[name] = value));
   console.log(department);
 
   axios.post(`${url}/department`, department, header).then((res) => {
+    info.innerHTML = `
+    <div class="alert alert-success" role="alert">Yangi bo'lim qo'shildi</div>`;
+    setTimeout(() => {
+      info.innerHTML = "";
+    }, 2000);
     departments = [res.data, ...departments];
     form.reset();
     render();
